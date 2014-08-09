c89f8190e8aff140831808d81f96ef4d20b4a9cd
richard-aes <richard@associatedenvironmentalsystems.com>
2014-08-09 09:26:47 -0400

    HTML base
    Built out index.php with basic structure.

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li>
+					<a href="#">Home</a>
+				</li>
+				<li>
+					<a href="#">Products</a>
+				</li>
+				<li>
+					<a href="#">About</a>
+				</li>
+				<li>
+					<a href="#">Blog</a>
+				</li>
+				<li>
+					<a href="#">Contact</a>
+				</li>
+			</ul>
+		</nav>
+	</header>
+	<main>
+		<section id="home" class="slide"></section>
+		<section id="products" class="slide"></section>
+		<section id="about" class="slide"></section>
+		<section id="blog" class="slide"></section>
+		<section id="contact" class="slide"></section>
+	</main>
+	<footer>
+		&copy; Finix, <?php echo date("Y"); ?>.
+	</footer>
+</body>
+</html>
