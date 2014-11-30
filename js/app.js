75b374eb70cd69562c411f05248c184eac690c05
finix-rsmith <rsmith@finixcreative.com>
2014-11-30 15:53:28 -0500

    Team - refactor controller
    Remove tabs logic from team controller

--- js/app.js
+++ js/app.js
@@ -17,13 +17,6 @@
 
 	app.controller('TeamController', function(){
 		this.products = team;
-		this.tab = 1;
-		this.selectTab = function(setTab){
-			this.tab = setTab;
-		};
-		this.isSelected = function(checkTab){
-			return this.tab === checkTab;
-		};
 	});
 
 	app.controller('ArticlesController', function(){
