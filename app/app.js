21a361763ab4297cb13eee60abed4320484e53c7
finix-rsmith <rsmith@finixcreative.com>
2015-03-02 14:46:28 -0500

    App - directives dependency
    Remove dependency

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-angular.module('websiteApp', ['ngRoute', 'ngAnimate', 'websiteControllers', 'websiteServices', 'websiteDirectives'])
+angular.module('websiteApp', ['ngRoute', 'ngAnimate', 'websiteControllers', 'websiteServices'])
 	.config(['$routeProvider', function($routeProvider) {
 		$routeProvider
 			.when('/', {
