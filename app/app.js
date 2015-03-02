c719a7573092fb6b4442d56b6293b0f570453331
finix-rsmith <rsmith@finixcreative.com>
2015-03-02 15:57:08 -0500

    Routes - controller aliases
    Add "controllerAs" attribute for each route view

--- app/app.js
+++ app/app.js
@@ -6,11 +6,13 @@ angular.module('websiteApp', ['ngRoute', 'ngAnimate', 'websiteControllers', 'web
 			})
 			.when('/products', {
 				templateUrl: 'app/pages/products/products.html',
-				controller: 'productsController'
+				controller: 'productsController',
+				controllerAs: 'products'
 			})
 			.when('/products/:productId', {
 				templateUrl: 'app/pages/products/views/product.html',
 				controller: 'productDetailController'
+				controllerAs: 'productdetails'
 			})
 			.when('/support', {
 				templateUrl: 'app/pages/support/support.html',
@@ -18,14 +20,17 @@ angular.module('websiteApp', ['ngRoute', 'ngAnimate', 'websiteControllers', 'web
 			.when('/articles', {
 				templateUrl: 'app/pages/articles/articles.html',
 				controller: 'articlesController'
+				controllerAs: 'articles'
 			})
 			.when('/about', {
 				templateUrl: 'app/pages/about/about.html',
 				controller: 'teamController'
+				controllerAs: 'profiles'
 			})
 			.when('/about/:profileId', {
 				templateUrl: 'app/pages/about/profile.html',
 				controller: 'teamController'
+				controllerAs: 'profiledetails'
 			})
 			.when('/contact', {
 				templateUrl: 'app/pages/contact/contact.html',
