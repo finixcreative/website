dd4699dacd75bd0cdc856f0e6ba9130d0498f9eb
finix-rsmith <rsmith@finixcreative.com>
2015-03-02 14:01:30 -0500

    Products - service variable
    Refactor products service to fetch main JSON array

--- app/pages/products/products-srv.js
+++ app/pages/products/products-srv.js
@@ -1,8 +1,9 @@
 angular.module('websiteServices', ['ngResource'])
 	.factory('Products', ['$resource', function ProductsFactory($resource){
-		return $resource('app/pages/products/products/', {}, {
+		return $resource('app/pages/products/products/:productId.json', {}, {
 			query: {
 				method: 'GET',
+				params: {productId: 'products'},
 				isArray: true
 			}
 		});
