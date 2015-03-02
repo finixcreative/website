4a75f8f318c53e401c852e7c17ca2e4f4f5d1f79
finix-rsmith <rsmith@finixcreative.com>
2015-03-02 14:00:58 -0500

    Slides - refactor
    Clean up controller and view

--- app/widgets/slides/slides-ctrl.js
+++ app/widgets/slides/slides-ctrl.js
@@ -1,4 +1,4 @@
-angular.module('websiteControllers', ['ngAnimate', 'ngRoute'])
+angular.module('websiteControllers')
 	.controller('slidesController', ['$scope', 'Slides', function($scope, Slides) {
 		$scope.images = Slides.query();
 	}]);
