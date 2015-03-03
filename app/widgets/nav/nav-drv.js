f4a5c0de9e7fe95b61cdb42627cdfe65a8c47af5
finix-rsmith <rsmith@finixcreative.com>
2015-03-03 17:35:33 -0500

    Directives - fix paths
    Set paths to directive templates

--- app/widgets/nav/nav-drv.js
+++ app/widgets/nav/nav-drv.js
@@ -2,6 +2,6 @@ angular.module('navMod', [])
 	.directive('navigation', function(){
 		return {
 			restrict: 'E',
-			template: 'app/widgets/nav/nav.html'
+			template: '/app/widgets/nav/nav.html'
 		};
 	});
