0ad03e7cdfee13380cfee4645af3241702482b58
finix-rsmith <rsmith@finixcreative.com>
2015-03-03 21:15:33 -0500

    Footer - templateUrl
    Set attribute name to templateUrl

--- app/widgets/footer/footer-drv.js
+++ app/widgets/footer/footer-drv.js
@@ -2,6 +2,6 @@ angular.module('footerMod', [])
 	.directive('appfooter', function(){
 		return {
 			restrict: 'E',
-			template: '/website/app/widgets/footer/footer.html'
+			templateUrl: '/website/app/widgets/footer/footer.html'
 		};
 	});
