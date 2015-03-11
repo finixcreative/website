4795899f65a755dcf40ca7b087f3557a8142d80c
finix-rsmith <rsmith@finixcreative.com>
2015-03-11 16:05:46 -0400

    Profiles - controller
    Update controller to use $http get

--- app/pages/about/profiles-ctrl.js
+++ app/pages/about/profiles-ctrl.js
@@ -1,5 +1,12 @@
 angular.module('profilesMod')
-	.controller('ProfilesController', ['$scope', 'Profiles', function($scope, Profiles) {
-	//	$scope.profiles = Profiles.query();
-		$scope.profiles = profiles;
+//	.controller('ProfilesController', ['$scope', 'Profiles', function($scope, Profiles) {
+//	//	$scope.profiles = Profiles.query();
+//		$scope.profiles = profiles;
+//	}]);
+	.controller('ProfilesController', [ '$http', function($http){
+		var about = this;
+		about.profiles = [];
+		$http.get('app/pages/products/products/products.json').success(function(data){
+			about.profiles = data;
+		});
 	}]);
