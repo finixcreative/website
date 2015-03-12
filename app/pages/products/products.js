51b1449fd27c6513e5c65f79efc008cbd9695f58
finix-rsmith <rsmith@finixcreative.com>
2015-03-11 22:25:52 -0400

    Products - social media text
    Update social media product description

--- app/pages/products/products.js
+++ app/pages/products/products.js
@@ -57,6 +57,6 @@ var products = [
 			"src": "assets/images/products/social.jpg",
 			"alt": "Social Product background",
 		},
-		"shortdescription": "Social Media is a great way to connect with customers and promote your business. A study of several Wall Street businesses found a 99% correlation between their number of social media "likes" and the value of each stock. Every business has something to gain from building a social media presence!",
+		"shortdescription": "Social Media is a great way to connect with customers and promote your business. A study of several Wall Street businesses found a 99% correlation between their number of social media followers and the value of each stock. Every business has something to gain from building a social media presence!",
 	},
 ];
