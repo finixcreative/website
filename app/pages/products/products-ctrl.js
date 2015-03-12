fbf6465ecea096d74ec1ef817e3bd520329b351d
finix-rsmith <rsmith@finixcreative.com>
2015-03-11 20:41:16 -0400

    Products - hail mary
    Last attempt to connect remote data to controller

--- app/pages/products/products-ctrl.js
+++ app/pages/products/products-ctrl.js
@@ -3,10 +3,6 @@ angular.module('productsMod')
 //		$scope.products = Products.query();
 //	}]);
 
-	.controller('ProductsController', [ '$http', function($http){
-		var store = this;
-		store.products = [];
-		$http.get('app/pages/products/products/products.json').success(function(data){
-			store.products = data;
-		});
+	.controller('ProductsController', function(){
+		this.products = products;
 	}]);
