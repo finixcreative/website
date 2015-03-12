72c55df4b7fb8ed98a7c212121af8486d3e97dee
finix-rsmith <rsmith@finixcreative.com>
2015-03-11 22:21:45 -0400

    Products - update tiles
    Fix tile content

--- app/pages/products/products.js
+++ app/pages/products/products.js
@@ -9,27 +9,54 @@ var products = [
 			"src": "assets/images/products/web.jpg",
 			"alt": "Web Product background",
 		},
-		"url": "#/products/web",
 		"shortdescription": "In the internet-driven marketplace, every business needs a professional website to stay in the game. Without a strong online presence, many potential customers will never find your business.",
 	},
 	{
-		"id": "brand",
-		"name": "Brand",
-		"background": "assets/images/products/brand.jpg",
+		"name": "brand",
+		"icon": {
+			"src": "assets/images/icons/icon-brand.png",
+			"alt": "Brand Product icon",
+		},
+		"background": {
+			"src": "assets/images/products/brand.jpg",			
+			"alt": "Brand Product background",
+		},
+		"shortdescription": "The first step for any new company! Logo creation and brand identity development give your fledgeling business a professional look.",
 	},
 	{
-		"id": "email",
-		"name": "Email",
-		"background": "assets/images/products/email.jpg",
+		"name": "email",
+		"icon": {
+			"src": "assets/images/icons/icon-email.png",
+			"alt": "Email Product icon",
+		},
+		"background": {
+			"src": "assets/images/products/email.jpg",
+			"alt": "Email Product icon",
+		},
+		"shortdescription": "The quickest way to connect with new customers and drive traffic to your website is with a targeted email marketing campaign, or "email blast". If you are promoting a special deal, a new product or service, or just trying to build up a customer base, we give you the tools you need to reach your goals.",
 	},
 	{
-		"id": "print",
-		"name": "Print",
-		"background": "assets/images/products/print.jpg",
+		"name": "print",
+		"icon": {
+			"src": "assets/images/icons/icon-print.png",
+			"alt": "Print Product icon",
+		},
+		"background": {
+			"src": "assets/images/products/print.jpg",
+			"alt": "Print Product background",
+		},
+		"shortdescription": "Complete your branding package with custom printed materials for local networking needs. From business cards to advertisement design, we can provide you with professional, eye-catching designs for all of your needs.",
 	},
 	{
-		"id": "social",
-		"name": "Social",
-		"background": "assets/images/products/social.jpg",
+		"name": "social",
+		"icon": {
+			"src": "assets/images/icons/icon-social.png",
+			"alt": "Social Product icon",
+		},
+		"background": {
+			"src": "assets/images/products/social.jpg",
+			"alt": "Social Product background",
+		},
+		"shortdescription": "Social Media is a great way to connect with customers and promote your business. A study of several Wall Street businesses found a 99% correlation between their number of social media "likes" and the value of each stock. Every business has something to gain from building a social media presence!",
 	},
 ];
