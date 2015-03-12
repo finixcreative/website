6a8a569cced5acfd78f1de16c066c73a12357d2f
finix-rsmith <rsmith@finixcreative.com>
2015-03-12 15:00:58 -0400

    Articles - fix tags
    Set up correct bindings and fix JSON structure

--- app/pages/articles/articles.js
+++ app/pages/articles/articles.js
@@ -2,7 +2,10 @@ var articles = [
 	{
 		"headline": "Branding",
 		"subheadline": "Identity by design",
-		"author": "Richard Smith",
+		"author": {
+			"name": "Richard Smith",
+			"link": "#/about/rsmith",
+		},
 		"image": "assets/images/article-branding.jpg",
 		"content": "Lorem ipsum dolor sit amet, dui molestie tincidunt suscipit ipsum sem laoreet, cum sollicitudin massa luctus, suspendisse urna enim sit lobortis dolor, vitae eget tortor habitant eget, sodales lorem. Suscipit magna porttitor ut, vestibulum fusce error nascetur donec massa risus. Facilisi vivamus semper mauris, eget fusce et nostra sit, est euismod dolor, non cubilia curabitur unde facilisis amet rhoncus, sem eros enim tempus volutpat. Lacinia ipsum odio ac feugiat, donec dolor ipsum lectus consequat in, hymenaeos a ac, in neque, tristique sed faucibus elit. Mauris purus augue vestibulum, ac eros nihil egestas dictum cum vel, leo vitae vestibulum, elementum convallis dolor, vel sollicitudin a pellentesque. Et fames, a taciti porttitor lorem diam, urna ac, dui tincidunt amet turpis sit, est purus eget. Et congue et consectetuer. Molestie fermentum vulputate, feugiat turpis sollicitudin, laoreet ac amet enim et blandit.",
 		"date": "7.30.2014",
@@ -10,7 +13,10 @@ var articles = [
 	{
 		"headline": "Instagram Tips",
 		"subheadline": "",
-		"author": "Allison Vollmar",
+		"author": {
+			"name": "Allison Vollmar",
+			"link": "http://www.linkedin.com/pub/allison-vollmar/8/ab2/610",
+		},
 		"image": "assets/images/article-instagram_tips.jpg",
 		"content": "Lorem ipsum dolor sit amet, dui molestie tincidunt suscipit ipsum sem laoreet, cum sollicitudin massa luctus, suspendisse urna enim sit lobortis dolor, vitae eget tortor habitant eget, sodales lorem. Suscipit magna porttitor ut, vestibulum fusce error nascetur donec massa risus. Facilisi vivamus semper mauris, eget fusce et nostra sit, est euismod dolor, non cubilia curabitur unde facilisis amet rhoncus, sem eros enim tempus volutpat. Lacinia ipsum odio ac feugiat, donec dolor ipsum lectus consequat in, hymenaeos a ac, in neque, tristique sed faucibus elit. Mauris purus augue vestibulum, ac eros nihil egestas dictum cum vel, leo vitae vestibulum, elementum convallis dolor, vel sollicitudin a pellentesque. Et fames, a taciti porttitor lorem diam, urna ac, dui tincidunt amet turpis sit, est purus eget. Et congue et consectetuer. Molestie fermentum vulputate, feugiat turpis sollicitudin, laoreet ac amet enim et blandit.",
 		"date": "8.10.2014",
@@ -18,7 +24,10 @@ var articles = [
 	{
 		"headline": "Desktop Publishing",
 		"subheadline": "",
-		"author": "Chris Baeckstrom",
+		"author": {
+			"name": "Chris Baeckstrom",
+			"link": "http://www.linkedin.com/pub/christopher-baeckstrom/74/b7a/548",
+		},
 		"image": "assets/images/article-instagram_tips.jpg",
 		"content": "Lorem ipsum dolor sit amet, dui molestie tincidunt suscipit ipsum sem laoreet, cum sollicitudin massa luctus, suspendisse urna enim sit lobortis dolor, vitae eget tortor habitant eget, sodales lorem. Suscipit magna porttitor ut, vestibulum fusce error nascetur donec massa risus. Facilisi vivamus semper mauris, eget fusce et nostra sit, est euismod dolor, non cubilia curabitur unde facilisis amet rhoncus, sem eros enim tempus volutpat. Lacinia ipsum odio ac feugiat, donec dolor ipsum lectus consequat in, hymenaeos a ac, in neque, tristique sed faucibus elit. Mauris purus augue vestibulum, ac eros nihil egestas dictum cum vel, leo vitae vestibulum, elementum convallis dolor, vel sollicitudin a pellentesque. Et fames, a taciti porttitor lorem diam, urna ac, dui tincidunt amet turpis sit, est purus eget. Et congue et consectetuer. Molestie fermentum vulputate, feugiat turpis sollicitudin, laoreet ac amet enim et blandit.",
 		"date": "8.10.2014",
