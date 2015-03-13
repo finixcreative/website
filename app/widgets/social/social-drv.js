8c1fc382295ed5226d2b376b2fe4d33b1471c8b5
finix-rsmith <rsmith@finixcreative.com>
2015-03-13 01:21:54 -0400

    Social - fix directive
    Add missing comma

--- app/widgets/social/social-drv.js
+++ app/widgets/social/social-drv.js
@@ -2,7 +2,7 @@ angular.module('socialMod')
 	.directive('social', function(){
 		return {
 			restrict: 'E',
-			templateUrl: '/website/app/widgets/social/social.html'
+			templateUrl: '/website/app/widgets/social/social.html',
 			controller: 'SocialController',
 			controllerAs: 'social',
 		};
