bc43ff758238958ed529dca3afdfafe6973606a5
finix-rsmith <rsmith@finixcreative.com>
2015-03-13 16:27:36 -0400

    Revert "Slides - add product slides"
    This reverts commit 6e6d71e2a026af527a2b399e8a1b2b13c76c4bbd.

--- app/widgets/slides/slides.js
+++ app/widgets/slides/slides.js
@@ -18,7 +18,7 @@ var images = [
 		],
 	},
 	{
-		"background": "assets/images/background-boston_skyline.png",
+		"background": "assets/images/iStock_000013899547Large.jpg",
 		"position": "center",
 		"icon": {
 			"src": "assets/images/icons/icon-web.png",
@@ -30,80 +30,8 @@ var images = [
 		"text": "Lorem ipsum dolor sit amet, wisi ac hymenaeos elementum, est etiam vitae quam proin, eleifend donec arcu ut dolor, volutpat platea metus dictum elementum at eu, lacus etiam bibendum placerat conubia consectetuer.",
 		"links": [
 			{
-				"text": "Get a website >",
-				"url": "#/products/web",
-			},
-		],
-	},
-	{
-		"background": "assets/images/background-boston_skyline.png",
-		"position": "center",
-		"icon": {
-			"src": "assets/images/icons/icon-brand.png",
-			"alt": "Brand icon",
-		},
-		"id": "slide2",
-		"headline": "Title",
-		"subheadline": "Subtitle",
-		"text": "Lorem ipsum dolor sit amet, wisi ac hymenaeos elementum, est etiam vitae quam proin, eleifend donec arcu ut dolor, volutpat platea metus dictum elementum at eu, lacus etiam bibendum placerat conubia consectetuer.",
-		"links": [
-			{
-				"text": "Get a logo >",
-				"url": "#/products/brand",
-			},
-		],
-	},
-	{
-		"background": "assets/images/iStock_000013899547Large.jpg",
-		"position": "center",
-		"icon": {
-			"src": "assets/images/icons/icon-email.png",
-			"alt": "Email icon",
-		},
-		"id": "slide2",
-		"headline": "Title",
-		"subheadline": "Subtitle",
-		"text": "Lorem ipsum dolor sit amet, wisi ac hymenaeos elementum, est etiam vitae quam proin, eleifend donec arcu ut dolor, volutpat platea metus dictum elementum at eu, lacus etiam bibendum placerat conubia consectetuer.",
-		"links": [
-			{
-				"text": "Start an email campaign >",
-				"url": "#/products/email",
-			},
-		],
-	},
-	{
-		"background": "assets/images/iStock_000013899547Large.jpg",
-		"position": "center",
-		"icon": {
-			"src": "assets/images/icons/icon-print.png",
-			"alt": "Print icon",
-		},
-		"id": "slide2",
-		"headline": "Title",
-		"subheadline": "Subtitle",
-		"text": "Lorem ipsum dolor sit amet, wisi ac hymenaeos elementum, est etiam vitae quam proin, eleifend donec arcu ut dolor, volutpat platea metus dictum elementum at eu, lacus etiam bibendum placerat conubia consectetuer.",
-		"links": [
-			{
-				"text": "Get printed items >",
-				"url": "#/products/print",
-			},
-		],
-	},
-	{
-		"background": "assets/images/background-boston_skyline.png",
-		"position": "center",
-		"icon": {
-			"src": "assets/images/icons/icon-social.png",
-			"alt": "Social icon",
-		},
-		"id": "slide2",
-		"headline": "Title",
-		"subheadline": "Subtitle",
-		"text": "Lorem ipsum dolor sit amet, wisi ac hymenaeos elementum, est etiam vitae quam proin, eleifend donec arcu ut dolor, volutpat platea metus dictum elementum at eu, lacus etiam bibendum placerat conubia consectetuer.",
-		"links": [
-			{
-				"text": "Connect with your customers >",
-				"url": "#/products/social",
+				"text": "Read More >",
+				"url": "#/blog",
 			},
 		],
 	},
