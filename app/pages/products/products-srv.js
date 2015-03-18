9e583980994fbbf000929bcad324513720e50d10
finix-rsmith <rsmith@finixcreative.com>
2015-03-18 09:19:43 -0400

    Products - factory
    Comment out factory in productsMod

--- app/pages/products/products-srv.js
+++ app/pages/products/products-srv.js
@@ -1,10 +1,10 @@
 angular.module('productsMod', ['ngResource'])
-	.factory('Products', ['$resource', function($resource){
-		return $resource('/website/app/pages/products/products/:productId.json', {}, {
-			query: {
-				method: 'GET',
-				params: {productId: 'products'},
-				isArray: true
-			}
-		});
-	}]);
+	//.factory('Products', ['$resource', function($resource){
+	//	return $resource('/website/app/pages/products/products/:productId.json', {}, {
+	//		query: {
+	//			method: 'GET',
+	//			params: {productId: 'products'},
+	//			isArray: true
+	//		}
+	//	});
+	//}]);
